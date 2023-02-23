1365f8f6c404d0a56f26879e422ac0eee857316a
unknown <57758gfk@gmail.com>
2023-02-23 16:40:05 +0200

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,17 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    int hrn, kop;
+    cout << "enter -> ";
+    cin >> hrn >> kop;
+
+    if (kop > 99) {
+        hrn += kop / 100;
+        kop %= 100;
+    }
+
+    cout << "corrected amount: " << hrn << " hrn " << kop << " kop\n";
+
+    return 0;
 }
