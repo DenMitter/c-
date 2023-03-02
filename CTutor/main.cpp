cc0efd68a787bdc7911699d4ee8a4ba12a6b905c
unknown <57758gfk@gmail.com>
2023-03-02 17:34:51 +0200

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,26 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int num;
+    cout << "Введіть чотиризначне число: ";
+    cin >> num;
+
+    if (num < 1000 || num>9999) {
+        cout << "число не чотиризначне";
+        return 0;
+    }
+
+    int d1 = num / 1000 % 10,
+        d2 = num / 100 % 10,
+        d3 = num / 10 % 10,
+        d4 = num % 10;
+
+    int new_num = d4 * 1000 + d3 * 100 + d2 * 10 + d1;
+
+    cout << "число становить " << new_num << endl;
+
+    return 0;
 }
