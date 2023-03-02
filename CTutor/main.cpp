04999b7f17c0dfbc175beeadabc54dfc205317ee
unknown <57758gfk@gmail.com>
2023-03-02 16:19:41 +0200

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,20 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int start, end, num = 0;
+
+    cout << "start -> ";
+    cin >> start;
+    cout << "end -> ";
+    cin >> end;
+
+    while(start < end) {
+        cout << num << endl;
+        num += start;
+        start++;
+    }
+    cout << "Результат\t—\t" + num;
+    return 0;
 }
