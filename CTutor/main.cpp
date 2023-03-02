fe45b2a4a93aa4f1aafa75126020b01f9565ee5c
unknown <57758gfk@gmail.com>
2023-03-02 17:08:40 +0200

    Add dop

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,18 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int n, avg;
+    int sum = 0, i = 1, count = 0;
+
+    for (i = 1; i <= 1000; i++) {
+        sum += i;
+        count++;
+    }
+
+    avg = sum / count;
+    cout << "Середнє арифметичне всіх цілих чисел від 1 до 1000 є:" << avg << endl;
+    return 0;
 }
