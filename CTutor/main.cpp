58083f0a55c0314c1368cbc14d7f21edba76913a
unknown <57758gfk@gmail.com>
2023-03-02 16:33:43 +0200

    Add lab

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    int numOne, numTwo, result;
+    cout << "one -> ";
+    cin >> numOne;
+    cout << "two -> ";
+    cin >> numTwo;
+
+    if (numOne > numTwo) {
+        result = numTwo;
+    }
+    else {
+        result = numOne;
+    }
+
+    cout << endl << result;
+    return 0;
 }
