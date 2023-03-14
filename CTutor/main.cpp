6ffb7bbcdf4c4af20dcb5f842f1d1b878612a214
unknown <57758gfk@gmail.com>
2023-03-14 16:21:03 +0200

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    int arr[] = { 2, 0, 5, 7, 0, 6, 0, 8 };
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int j = 0;
+    for (int i = 0; i < length; i++) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+    for (int i = j; i < length; i++) {
+        arr[i] = -1;
+    }
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
