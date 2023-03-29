2f05d64d039c87222f763d2bb1c51daa13194327
unknown <57758gfk@gmail.com>
2023-03-29 11:34:09 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int function(int a, int b) {
+    if (a == 0) {
+        return b;
+    }
+    return function(b % a, a);
+}
+
+int main() {
+    cout << function(5, 10);
+    return 0;
 }
