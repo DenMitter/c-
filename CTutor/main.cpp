178dc37e7ade33d46543c5f79a405fd5a330c6fe
unknown <57758gfk@gmail.com>
2023-03-29 11:59:38 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,41 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int len(int a) {
+    if (a == 0) {
+        return 0;
+    }
+    else {
+        return 1 + len(a / 10);
+    }
+}
+
+int palindrom(int a, int k = 0, int c = 0, int b = 0) {
+    if (a == 0) {
+        return true;
+    }
+
+    k = len(a);
+    c = a / pow(10, k - 1);
+    b = a % 10;
+
+    if (c == b) {
+        return palindrom(a % int(pow(10, k - 1)) / 10);
+    }
+    else {
+        return false;
+    }
+}
+
+int main() {
+    int x = 95;
+
+    len(x);
+    if (palindrom(x)) {
+        cout << "yes" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
 }
