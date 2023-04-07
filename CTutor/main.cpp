376f49e7acd5b5ae25d34356d7a6317e50522faf
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-04-07 11:31:47 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,30 @@
 ﻿#include <iostream>
+
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int binarySearch(int arr[], int low, int high) {
+    int mid;
+    while (low < high) {
+        mid = (low + high) / 2;
+
+        if (arr[mid] > arr[mid + 1]) {
+            return mid;
+        }
+        else if (arr[mid] < arr[mid + 1]) {
+            low = mid + 1;
+        }
+        else {
+            high = mid;
+        }
+    }
+    return low;
+}
+
+int main() {
+    int arr[] = { 2, 5, 8, 12, 16, 23, 38, 56, 72, 91 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int max_index = binarySearch(arr, 0, n - 1);
+    cout << "res -> " << arr[max_index] << endl;
+    return 0;
 }
