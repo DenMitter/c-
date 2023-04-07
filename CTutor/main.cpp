52130cb2cb7251a8681817099767061fdbf48312
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-04-07 11:23:46 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,29 @@
 ﻿#include <iostream>
+
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+                swap(arr[min_idx], arr[i]);
+    }
+}
+
+int main() {
+    int arr[] = { 64, 25, 12, 22, 11 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
 }
