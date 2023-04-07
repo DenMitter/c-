4363dbb0b2b88bf000fcc43b993a4e82bda9fc5e
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-04-07 11:37:57 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,35 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+bool binarySearch(double arr[], int size, double x) {
+    int left = 0, right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == x) {
+            return true;
+        }
+        else if (arr[mid] < x) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
+
+int main() {
+    double arr[] = { 1.2, 3.4, 5.6, 7.8, 9.0 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    double x = 5.6;
+    bool found = binarySearch(arr, size, x);
+
+    if (found) {
+        cout << "is found -> " << x << endl;
+    }
+    else {
+        cout << "is not found -> " << x << endl;
+    }
+
+    return 0;
 }
