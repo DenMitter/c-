95ffd13d1dca8573789ff0a2229ff4dc93ae2439
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-04-07 11:35:26 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,49 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+void mergeArrays(int arr1[], int size1, int arr2[], int size2, int result[]) {
+    int i = 0, j = 0, k = 0;
+    while (i < size1 && j < size2) {
+        if (arr1[i] < arr2[j]) {
+            result[k] = arr1[i];
+            i++;
+        }
+        else {
+            result[k] = arr2[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < size1) {
+        result[k] = arr1[i];
+        i++;
+        k++;
+    }
+    while (j < size2) {
+        result[k] = arr2[j];
+        j++;
+        k++;
+    }
+}
+
+int main() {
+    int arr1[] = { 1, 3, 5, 7, 9 };
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+
+    int arr2[] = { 2, 4, 6, 8, 10 };
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    int* result = new int[size1 + size2];
+
+    mergeArrays(arr1, size1, arr2, size2, result);
+
+    cout << "res -> ";
+    for (int i = 0; i < size1 + size2; i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+
+    delete[] result;
+    return 0;
 }
