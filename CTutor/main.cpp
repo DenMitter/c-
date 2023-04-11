ff1c632029404f6dcef01244e633e32f78e1ed63
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-04-11 16:58:56 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,43 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+double celsiusToFahrenheit(double celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+void convertTemperature(double temperature, double (*convertFunc)(double)) {
+    double result = convertFunc(temperature);
+    cout << "result ->  " << result << endl;
+}
+
+int main() {
+    double temperature;
+    int choice;
+
+    cout << "input -> ";
+    cin >> temperature;
+
+    cout << "1. Celcia to Farengeit\n";
+    cout << "2. Farengeit to Celcia\n";
+    cin >> choice;
+
+    switch (choice) {
+    case 1: {
+        convertTemperature(temperature, celsiusToFahrenheit);
+        break;
+    }
+    case 2: {
+        convertTemperature(temperature, fahrenheitToCelsius);
+        break;
+    }
+    default: {
+        cout << "Error\n";
+        break;
+    }
+    }
+    return 0;
 }
