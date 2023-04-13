5240c93fb457fd07e0651acbf25323259c297194
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-04-13 16:01:31 +0300

    Add lab

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,23 @@
 ﻿#include <iostream>
+#include <string>
+
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    string str = "string and st35ring";
+    int digits = 0;
+    int letters = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (isdigit(str[i])) {
+            digits++;
+        }
+        if (isalpha(str[i])) {
+            letters++;
+        }
+    }
+
+    cout << "leters -> " << letters << endl;
+    cout << "digits -> " << digits;
+    return 0;
 }
