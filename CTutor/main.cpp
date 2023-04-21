187dfc68ae1deb04153824aba3dc7d7a50bc4c17
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-04-21 09:50:52 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,60 @@
 ﻿#include <iostream>
-using namespace std;
+#include <string>
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+
+int mystrlen(char* str) {
+    int len = 0;
+    while (*str != '\0') {
+        len++;
+        str++;
+    }
+    return len;
+}
+
+char* mystrcpy(char* str1, const char* str2) {
+    char* original_str1 = str1;
+    while (*str2 != '\0') {
+        *str1 = *str2;
+        str1++;
+        str2++;
+    }
+    *str1 = '\0';
+    return original_str1;
+}
+
+char* mystrcat(char* str1, const char* str2) {
+    char* original_str1 = str1;
+    while (*str1 != '\0') {
+        str1++;
+    }
+    while (*str2 != '\0') {
+        *str1 = *str2;
+        str1++;
+        str2++;
+    }
+    *str1 = '\0';
+    return original_str1;
+}
+
+char* mystrchr(char* str, char s) {
+    while (*str != '\0') {
+        if (*str == s) {
+            return str;
+        }
+        str++;
+    }
+    return NULL;
+}
+
+int main() {
+    char str1[100] = "Hello";
+    char str2[] = " world!";
+
+    printf("%d\n", mystrlen(str1));
+    printf("%s\n", mystrcpy(str1, str2));
+    printf("%s\n", mystrcpy(str1, str2));
+    printf("%s\n", mystrcat(str1, str2));
+    printf("%s\n", mystrchr(str1, 'o'));
+
+    return 0;
 }
