1939b29ef5573d9a9af48f4c87887e071a74dd0e
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-05-02 17:57:06 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,69 @@
-﻿#include <iostream>
+﻿// Я надіюсь ви не забули аудіо файл закинути на робочий стіл. І нажаль по другому не виходить запустити аудіо, тільки через відкриття якогось mp3 програвача
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+struct Animal {
+private:
+    string name;
+    string _class;
+    string alias;
+
+public:
+    Animal(string name, string _class, string alias) {
+        this->name = name;
+        this->_class = _class;
+        this->alias = alias;
+
+        cout << endl << "Create animal (argument)" << endl;
+    }
+    ~Animal() {
+        cout << endl << "Destroy animal";
+    }
+
+    void ser_name(string name) {
+        this->name = name;
+    }
+    void ser__class(string _class) {
+        this->name = _class;
+    }
+    void ser_alias(string alias) {
+        this->name = alias;
+    }
+
+    void get_name() const {
+        cout << endl << this->name;
+    }
+    void get__class() const {
+        cout << endl << this->_class;
+    }
+    void get_alias() const {
+        cout << endl << this->alias;
+    }
+
+    void vote() {
+        HINSTANCE result;
+        result = ShellExecute(NULL, NULL, L"C:/Users/Home/Desktop/57647-napadenie-na-belarus.mp3", NULL, NULL, SW_SHOWDEFAULT);// Home замінити на свого користувача
+        if ((int)result <= 32) {
+            cout << "Error\nReturn value: " << (int)result << endl;
+        }
+    }
+};
+
+int main() {
+    int punkt = 0;
+    Animal animal("bel", "undefined", "bulba");
+
+    animal.get_name();
+    animal.get__class();
+    animal.get_alias();
+
+    cout << "Vote in bulba - input \"1\"\t";
+    cin >> punkt;
+
+    if (punkt == 1) { animal.vote(); }
+    else { cout << "Error"; }
+    return 0;
 }
