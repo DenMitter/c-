a05dea603dcadef22ea7996dd580228557bb37c1
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-05-02 17:02:00 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,44 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+struct WashingMachine {
+    string brand;
+    string color;
+    float width;
+    float length;
+    float height;
+    int power;
+    int spin_speed;
+    int temperature;
+
+    WashingMachine(string brand, string color, float width, float length, float height, int power, int spin_speed, int temperature) {
+        this->brand = brand;
+        this->color = color;
+        this->width = width;
+        this->length = length;
+        this->height = height;
+        this->power = power;
+        this->spin_speed = spin_speed;
+        this->temperature = temperature;
+
+        cout << endl << "Create washing (argument)" << endl;
+    }
+    ~WashingMachine() {
+        cout << endl << "Destroy washing";
+    }
+};
+
+int main() {
+    WashingMachine washing_machine("Samsung", "White", 65.0, 65.0, 75.0, 1100, 3000, 41);
+
+    cout << endl << "My washing machine:" << endl;
+    cout << "Brand: " << washing_machine.brand << endl;
+    cout << "Color: " << washing_machine.color << endl;
+    cout << "Dimensions: " << washing_machine.width << "x" << washing_machine.length << "x" << washing_machine.height << " cm" << endl;
+    cout << "Power: " << washing_machine.power << " W" << endl;
+    cout << "Spin speed: " << washing_machine.spin_speed << " rpm" << endl;
+    cout << "Temperature: " << washing_machine.temperature << " C" << endl;
+
+    return 0;
 }
