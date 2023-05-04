76a3a0daa4b163c7fd9c8480a029cef47d5b1a92
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-05-04 08:27:31 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,25 @@
 ﻿#include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+struct Point {
+    double x;
+    double y;
+};
+
+double distance(Point p1, Point p2) {
+    double dx = p1.x - p2.x;
+    double dy = p1.y - p2.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+int main() {
+    Point p1 = { 1.0, 2.0 };
+    Point p2 = { 3.0, 4.0 };
+
+    double d = distance(p1, p2);
+    cout << "Distance between (" << p1.x << ", " << p1.y << ") and (" << p2.x << ", " << p2.y << "): " << d << endl;
+
+    return 0;
 }
