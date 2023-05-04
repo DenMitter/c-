8a04bbfe24e13f513157caf9834a30e7831417a8
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-05-04 08:31:53 +0300

    Add hw

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,85 @@
 ﻿#include <iostream>
+
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+struct Fraction {
+    int numerator;
+    int denominator;
+};
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+void simplify(Fraction& f) {
+    int d = gcd(f.numerator, f.denominator);
+    f.numerator /= d;
+    f.denominator /= d;
+}
+
+Fraction add(Fraction f1, Fraction f2) {
+    Fraction result;
+    result.numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+    result.denominator = f1.denominator * f2.denominator;
+    simplify(result);
+    return result;
+}
+
+Fraction subtract(Fraction f1, Fraction f2) {
+    Fraction result;
+    result.numerator = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+    result.denominator = f1.denominator * f2.denominator;
+    simplify(result);
+    return result;
+}
+
+Fraction multiply(Fraction f1, Fraction f2) {
+    Fraction result;
+    result.numerator = f1.numerator * f2.numerator;
+    result.denominator = f1.denominator * f2.denominator;
+    simplify(result);
+    return result;
+}
+
+Fraction divide(Fraction f1, Fraction f2) {
+    Fraction result;
+    result.numerator = f1.numerator * f2.denominator;
+    result.denominator = f1.denominator * f2.numerator;
+    simplify(result);
+    return result;
+}
+
+void print(Fraction f) {
+    cout << f.numerator << "/" << f.denominator;
+}
+
+int main() {
+    Fraction f1 = { 3, 4 };
+    Fraction f2 = { 1, 2 };
+
+    Fraction sum = add(f1, f2);
+    Fraction difference = subtract(f1, f2);
+    Fraction product = multiply(f1, f2);
+    Fraction quotient = divide(f1, f2);
+
+    cout << "Sum: ";
+    print(sum);
+    cout << endl;
+
+    cout << "Difference: ";
+    print(difference);
+    cout << endl;
+
+    cout << "Product: ";
+    print(product);
+    cout << endl;
+
+    cout << "Quotient: ";
+    print(quotient);
+    cout << endl;
+
+    return 0;
 }
