b777ead4c7a6a634dc5b68aec3ac9b267a25891d
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-05-09 17:28:15 +0300

    Add lab

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,37 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    ifstream input_file("input.txt");
+    ofstream output_file("output.txt");
+
+    if (!input_file.is_open()) {
+        cout << "Error opening input file" << endl;
+        return 1;
+    }
+
+    if (!output_file.is_open()) {
+        cout << "Error opening output file" << endl;
+        return 1;
+    }
+
+    string lines[1000];
+    int num_lines = 0;
+    string line;
+    while (getline(input_file, line)) {
+        lines[num_lines] = line;
+        num_lines++;
+    }
+
+    for (int i = num_lines - 1; i >= 0; i--) {
+        output_file << lines[i] << endl;
+    }
+
+    input_file.close();
+    output_file.close();
+
+    return 0;
 }
