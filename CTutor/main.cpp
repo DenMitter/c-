1c24209c209d73e26cc1892db459329214679712
Zhizh_hm41@student.itstep.org <Zhizh_hm41@student.itstep.org>
2023-05-09 17:47:03 +0300

    Add lab

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,11 +1,43 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-	setlocale(LC_ALL, "Ukrainian");
-	cout << "Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтaрi рани заростуть\n\
-	Давай на чистоту, сядем разом на мосту\n\
-	I побазарим зараз тут хай cтapi рани заростуть";
-	return 0;
+int main() {
+    ifstream input_file("input.txt");
+    ofstream output_file("output.txt");
+
+    if (!input_file.is_open()) {
+        cout << "Error opening input file" << endl;
+        return 1;
+    }
+
+    if (!output_file.is_open()) {
+        cout << "Error opening output file" << endl;
+        return 1;
+    }
+
+    string line;
+    string last_line_without_spaces;
+    bool found_line_without_spaces = false;
+    while (getline(input_file, line)) {
+        output_file << line << endl;
+        if (line.find(' ') == string::npos) { // Якщо рядок не містить пробіли
+            last_line_without_spaces = line;
+            found_line_without_spaces = true;
+        }
+    }
+
+    if (found_line_without_spaces) {
+        output_file << "------------" << endl;
+    }
+    else {
+        output_file << endl << "------------" << endl;
+    }
+
+    input_file.close();
+    output_file.close();
+
+    return 0;
 }
